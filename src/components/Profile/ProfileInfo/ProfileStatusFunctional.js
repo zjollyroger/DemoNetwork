0b79db3775a22f97fa94895fb1ef83aa37a472be
johnny <the.procs@gmail.com>
2022-09-29 14:28:32 +0300

    Lesson 85 useEffect

--- src/components/Profile/ProfileInfo/ProfileStatusFunctional.js
+++ src/components/Profile/ProfileInfo/ProfileStatusFunctional.js
@@ -1,10 +1,14 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Preloader from "../../common/Preloader/Preloader";
 
 const ProfileStatusFunctional = (props) => {
 
     const [editMode, setEditMode] = useState(false);
     const [status, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status]);
 
     const activateEditMode = () => {
         setEditMode( true);
