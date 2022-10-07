ba602eb1bae7c193477e9a3b6257375284dcc9a2
johnny <the.procs@gmail.com>
2022-10-07 13:43:57 +0300

    Updates

---
+++ precache-manifest.2796da09fda94e8947030b7ce36d81fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d285e9e51cf70577ea67",
+    "url": "/demoNetwork/static/css/main.f913a9ca.chunk.css"
+  },
+  {
+    "revision": "d285e9e51cf70577ea67",
+    "url": "/demoNetwork/static/js/main.d285e9e5.chunk.js"
+  },
+  {
+    "revision": "e566ef5e4c5d50f8d512",
+    "url": "/demoNetwork/static/js/1.e566ef5e.chunk.js"
+  },
+  {
+    "revision": "d7ec1899787aa44b73e6",
+    "url": "/demoNetwork/static/css/2.57bee4f1.chunk.css"
+  },
+  {
+    "revision": "d7ec1899787aa44b73e6",
+    "url": "/demoNetwork/static/js/2.d7ec1899.chunk.js"
+  },
+  {
+    "revision": "a5d65b5ffad3ea1ea69b",
+    "url": "/demoNetwork/static/js/3.a5d65b5f.chunk.js"
+  },
+  {
+    "revision": "8d7f3b3fafd74adec685",
+    "url": "/demoNetwork/static/js/runtime~main.8d7f3b3f.js"
+  },
+  {
+    "revision": "b7f08d849960e4f35d1c4154ddd492ac",
+    "url": "/demoNetwork/static/media/faceoff.b7f08d84.jpg"
+  },
+  {
+    "revision": "502a11ad311653fdf001c2d46f227cf3",
+    "url": "/demoNetwork/static/media/loader.502a11ad.gif"
+  },
+  {
+    "revision": "9ae2bc650d9c3b3e5a176d377c698af5",
+    "url": "/demoNetwork/index.html"
+  }
+];
